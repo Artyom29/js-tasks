5a1b69f1dbbe7af00dc97e6b550ecb612794c02b
Artyom29 <113828199+Artyom29@users.noreply.github.com>
2022-10-09 12:51:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,142 @@
+// 1
 
-// 2 Запросите у пользователя его имя. Выведите в окошке (с помощью функции alert) 
-// фразу: Привет, Николай! (вместо Николай должно показываться имя, которое ввел пользователь)
-let user = prompt ('Ваше имя');
+// let varieble = 'Hidden'
+// if (varieble == 'Hidden'){
+//     alert("visible")
+// } else{
+//     alert("hidden")
+// }
 
-alert('Привет, ' + user)
+// 2
 
-// Задание 3
+// let number = 1
+// if (number == 0){
+//     alert(1)
+// }
 
-// Запросите у пользователя число. Затем запросите степень, в которую нужно возвести это число. Выведите в консоль результат.
+// if (number < 0){
+//     alert("less then zero")
+// }
 
+// if (number > 0){
+//     alert(number * 10)
+// }
 
-let number = prompt('Введите число');
-let degree = prompt('Введите степень');
-let answer = (number ** degree);
-alert(answer)
+// 3
+
+// let number = prompt('Введите число!');
+// let message = (number < 5) ? 0 :
+//     (number > 5) ? 1 ;
+
+// alert( message );
+
+// 4
+
+// let num1 = prompt('Введите первое число!');
+// let num2 = prompt('Введите второе число!');
+
+// if (num1 > num2){
+//     alert('Большее число:' + num1)
+//     console.log('Большее число:' + num1)
+// }
+
+// if (num2 > num1){
+//     alert('Большее число:' + num2)
+//     console.log('Большее число:' + num2)
+// }
+
+// if (num1 == num2){
+//     alert('Числа равны!')
+//     console.log('Числа равны!')
+// }
+
+// 5
+
+// let num1 = prompt('Введите первое число!');
+// let num2 = prompt('Введите второе число!');
+
+// if (num1 / num2){
+//     alert(num1 / num2)
+//     console.log(num1 / num2)
+// }
+
+// 6
+
+// let num1 = prompt('Введите средний балл!');
+
+// if (num1 <= 4){
+//     alert('Двоечник, иди учись!');
+// } else if (num1 <= 8){
+//     alert('Неплохо, но давай еще поднажмем!');
+// } else if (num1 <= 10){
+//     alert('Ого, да ты настоящий отличник!');
+// }  
+
+// 7
+
+// let num1 = prompt('Введите баллы за экзамены!');
+// let num2 = prompt('Введите количество выполненных проектов!');
+
+// if (num1 > 90 || num2 > 10){
+//     alert(100)
+//     console.log(100)
+// }else if (num1 > 75 && num2 >= 5){
+//     alert(90)
+//     console.log(90)
+// }else if (num1 > 50 && num2 >= 2){
+//     alert(75)
+//     console.log(75)
+// } else {
+//     alert(0)
+//     console.log(0)
+// }
+
+// 8
+
+// let days = prompt('Введите количество дней!');
+// let num1 = 40;
+// let num2 = 50;
+// let num3 = 20;
+//  if (days >= 7){
+//     alert(days * num1 - num2)
+//  } else if (days >= 3){
+//     alert(days * num1 - num3)
+//  } else{
+//     alert(days * num1)
+//  }
+
+// 9
+
+// for (let num=10; num < 10 ; num++){
+//    alert(num)
+//    console.log(num)
+// }
+
+// 10
+// for (let i = 0; i < 10; i += 2 ){
+//     let num = prompt('Введите число!');
+// if (num == 10){
+//     alert('Равно 10')
+// } else {
+//     alert('Не равно 10')
+// }
+// }
+
+// 11
+
+// let num = prompt('Введите число!')
+// for (let i = 0; i < num; i++){
+//     alert(i * i)
+//     console.log(i * i)
+// }
+
+// 12
+// for (let i = 0; i < 100; i++){
+//     if (i % 3){
+//         alert('Fizz')
+//     } else if (i % 5){
+//         alert('Buzz')
+//     } else if (i % 3 && i % 5){
+//         alert('FizzBuzz')
+//     }
+// }
